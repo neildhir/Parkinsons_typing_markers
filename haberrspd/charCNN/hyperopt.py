3cb496d9e0334ebe899d40a449b8d82e8f445f05
wagglefoot <neil.x.dhir@gmail.com>
2019-07-25 14:50:52 +0200

    backup main run script

---
+++ haberrspd/charCNN/hyperopt.py
@@ -0,0 +1,124 @@
+from pathlib import Path
+import datetime
+from sklearn.utils import class_weight
+import talos as ta
+from talos import Deploy, Predict, Restore
+import numpy as np
+from .models_tf import char_cnn_model_talos
+from .auxiliary_tf import create_training_data_keras
+from numpy import vstack, asarray
+import argparse
+
+
+from keras.optimizers import Adam, Nadam  # Which optimisers to consider
+
+# Global params live here
+from .globals import *  # TODO: this is bad syntax, fix this
+
+# This block is important if we want the memory to grow on the GPU, and not block allocate the whole thing
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+# --- PARSE ADDITIONAL USER SETTINGS
+
+parser = argparse.ArgumentParser(description='CNN text classifier.')
+# dataset
+parser.add_argument('-dataset',
+                    type=str,
+                    default="EnglishData-preprocessed.csv",
+                    help='Dataset to use for hyperparam optimisation [default is EnglishData-preprocessed.csv i.e. time + char information.]')
+args = parser.parse_args()
+
+
+# --- LOAD DATA
+
+DATA_ROOT = Path("../data/") / "MJFF" / "preproc"  # Note the relative path
+X_train, X_test, y_train, y_test, max_sentence_length, alphabet_size = \
+    create_training_data_keras(DATA_ROOT, args.dataset)  # XXX: set data as input to this file
+
+# Class weights are dynamic as the data-loader is stochastic and changes with each run.
+class_weights = dict(zip([0, 1],
+                         class_weight.compute_class_weight('balanced', list(set(y_train)), y_train)))
+
+
+# --- HYPERPARMETERS TO OPTIMISE
+
+"""
+Dynamic optimisation algoritns to choose from in keras.
+
+'sgd': SGD,
+'rmsprop': RMSprop,
+'adagrad': Adagrad,
+'adadelta': Adadelta,
+'adam': Adam,
+'adamax': Adamax,
+'nadam': Nadam
+"""
+
+
+optimisation_parameters = {
+    'lr': (0.1, 10, 3),  # Learning rate
+    'conv_output_space': [4],  # ,8],
+    'number_of_large_filters': [2],
+    'number_of_small_filters': [2],
+    'large_filter_length': [60],
+    'small_filter_length': [5],
+    'pool_length': [2],
+    'dense_units_layer_3': [32],
+    'dense_units_layer_2': [16],
+    'batch_size': [32],
+    'epochs': [100],
+    'dropout': [0.05],  # ,0.1,0.2],
+    'conv_kernel_initializer': ['uniform'],
+    'conv_bias_initializer': ['uniform'],
+    'dense_kernel_initializer': ['uniform'],
+    'dense_bias_initializer': ['uniform'],
+    'optimizer': [Adam],  # If used this way, these have to explicitly imported
+    'loss': ['binary_crossentropy'],  # Loss functions
+    'conv_activation': ['relu'],
+    'dense_activation': ['relu'],
+    'last_activation': ['sigmoid'],
+
+    # Stationary parameters, i.e. do not get optimised
+    'max_sentence_length': [max_sentence_length],
+    'alphabet_size': [alphabet_size],
+    'control_class_weight': [class_weights[0]],
+    'pd_class_weight': [class_weights[1]],
+}
+
+
+# --- RUN OPTIMISATION
+
+scanner = ta.Scan(x=X_train,
+                  y=asarray(y_train).reshape(-1, 1),
+                  x_val=X_test,
+                  y_val=asarray(y_test).reshape(-1, 1),
+                  model=char_cnn_model_talos,
+                  disable_progress_bar=False,
+                  params=optimisation_parameters)
+# grid_downsample=0.01,  # Randomly samples 1% of the grid
+
+# --- STORE RESULTS FOR FUTURE USE
+
+# Query best learned model
+probs = Predict(scanner)  # XXX: set options for the method properly
+
+# Extract class-probabilities from best learned model
+labels_and_label_probs = np.zeros((len(X_test), 2))
+for i, (y, x) in enumerate(zip(y_test, X_test)):
+    # Note that keras takes a 3D array and not the standard 2D, hence extra axis
+    labels_and_label_probs[i, :] = [y, float(probs.predict(x[np.newaxis, :, :]))]
+
+time_and_date = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+# Save array for later use
+np.savetxt("label_and_label_probs_" + time_and_date + ".csv",
+           labels_and_label_probs,
+           fmt='%.15f',
+           delimiter=",")
+
+# Save whole model for later use
+Deploy(scan_object=scanner, model_name='talos_best_model_' + time_and_date, metric='val_acc')
