4e99572bd5ce11a5ed3c6cc28758a3c992b05207
scc9 <medm@kth.se>
2020-11-05 16:21:07 +0100

    minor

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -6,13 +6,11 @@ Command line script for preprocessing execution
 """
 from src.preprocess import preprocessMRC
 
-
 def main():
 
     proc = preprocessMRC()
-    i = 'script'
     out = proc()
-    out.to_csv("data/MRC/preproc/char_time/EnglishData-preprocessed_attempt_{}.csv".format(i), index=False)
+    out.to_csv("data/MRC/preproc/EnglishData-preprocessed.csv", index=False)
 
 
 
