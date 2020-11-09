0b452fe7128a1cfb29b0396310d0c6ee6d955064
scc9 <medm@kth.se>
2020-11-09 13:06:51 +0100

    data download moved from dockerfile to bash script

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+wget https://kaminpublic.s3.eu-north-1.amazonaws.com/MedicationInfo.csv -P ./data/MRC/
+wget https://kaminpublic.s3.eu-north-1.amazonaws.com/MRCData-processed-interpolated.csv -P ./data/MRC/
+wget https://kaminpublic.s3.eu-north-1.amazonaws.com/mrc_fold_all.csv -P ./data/MRC/preproc/
