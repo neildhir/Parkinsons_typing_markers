82f72eec12fee59947de4f05dc1dcf8055b66dbe
Neil Dhir <ndhir@turing.ac.uk>
2020-11-11 18:42:16 +0000

    Test change

--- src/models.py
+++ src/models.py
@@ -15,31 +15,40 @@ from tensorflow.keras.layers import (
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 
-def mk_composite_model(input_shape,cfg,mode):
+
+def mk_composite_model(input_shape, cfg, mode):
     reg = cfg.train[mode].reg
     drop = cfg.train[mode].drop
 
     model = Sequential()
     model.add(Input(shape=input_shape))
-    model.add(Conv1D(filters = cfg.train.network.filters_1,
-                     kernel_size = cfg.train.network.kernel_size,
-                     strides = cfg.train.network.strides,
-                     padding= cfg.train.network.padding,
-                     activation=cfg.train.network.activation,
-                     name = 'conv1d_1',
-                     kernel_regularizer = regularizers.l2(reg),
-                     bias_regularizer = regularizers.l2(reg)))
+    model.add(
+        Conv1D(
+            filters=cfg.train.network.filters_1,
+            kernel_size=cfg.train.network.kernel_size,
+            strides=cfg.train.network.strides,
+            padding=cfg.train.network.padding,
+            activation=cfg.train.network.activation,
+            name="conv1d_1",
+            kernel_regularizer=regularizers.l2(reg),
+            bias_regularizer=regularizers.l2(reg),
+        )
+    )
     # model.add(BatchNormalization())
     model.add(Dropout(drop))
 
-    model.add(Conv1D(filters = cfg.train.network.filters_2,
-                     kernel_size = cfg.train.network.kernel_size,
-                     strides=cfg.train.network.strides,
-                     padding=cfg.train.network.padding,
-                     activation=cfg.train.network.activation,
-                     name = 'conv1d_2',
-                     kernel_regularizer = regularizers.l2(reg),
-                     bias_regularizer = regularizers.l2(reg)))
+    model.add(
+        Conv1D(
+            filters=cfg.train.network.filters_2,
+            kernel_size=cfg.train.network.kernel_size,
+            strides=cfg.train.network.strides,
+            padding=cfg.train.network.padding,
+            activation=cfg.train.network.activation,
+            name="conv1d_2",
+            kernel_regularizer=regularizers.l2(reg),
+            bias_regularizer=regularizers.l2(reg),
+        )
+    )
     # model.add(BatchNormalization())
     model.add(Dropout(drop))
     # model.add(Flatten())
