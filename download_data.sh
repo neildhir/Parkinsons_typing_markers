f533d1a953b9e6ae5bf6c63ed4b4a51c404458dc
scc9 <medm@kth.se>
2020-11-12 09:40:32 +0100

    changed data source

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 wget https://kaminpublic.s3.eu-north-1.amazonaws.com/MedicationInfo.csv -P ./data/MRC/
-wget https://kaminpublic.s3.eu-north-1.amazonaws.com/MRCData-processed-interpolated.csv -P ./data/MRC/
+wget https://kaminpublic.s3.eu-north-1.amazonaws.com/MRCData-processed-interpolated-public.csv -O ./data/MRC/MRCData-processed-interpolated.csv
 wget https://kaminpublic.s3.eu-north-1.amazonaws.com/mrc_fold_all.csv -P ./data/MRC/preproc/
 wget https://kaminpublic.s3.eu-north-1.amazonaws.com/cbow10_w3.wv -P ./misc/
